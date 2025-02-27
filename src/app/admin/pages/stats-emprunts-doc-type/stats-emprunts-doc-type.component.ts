ef7379227abbfd29a6cc70cfbe06e07063239267
LahmarTabai <Lahmartabai@yahoo.fr>
2025-02-28 00:19:38 +0100

    stats

--- src/app/admin/pages/stats-emprunts-doc-type/stats-emprunts-doc-type.component.ts
+++ src/app/admin/pages/stats-emprunts-doc-type/stats-emprunts-doc-type.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmpruntService } from '../../../services/emprunt.service';
 import { DocumentTypeStats } from '../../../models/emprunt.models';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-stats-emprunts-doc-type',
@@ -9,18 +12,28 @@ import { DocumentTypeStats } from '../../../models/emprunt.models';
 })
 export class StatsEmpruntsDocTypeComponent implements OnInit {
 
-  stats: DocumentTypeStats[] = [];
+  displayedColumns: string[] = ['docType', 'totalEmprunts'];
+  dataSource = new MatTableDataSource<DocumentTypeStats>();
   errorMessage = '';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private empruntService: EmpruntService) {}
 
   ngOnInit(): void {
     this.empruntService.getStatsEmpruntsByDocType().subscribe({
-      next: (data) => {
-        this.stats = data;
+      next: (data: DocumentTypeStats[]) => {
         if (data.length === 0) {
           this.errorMessage = 'Aucun résultat pour les stats.';
+        } else {
+          this.errorMessage = '';
         }
+        this.dataSource.data = data;
+        setTimeout(() => {
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        });
       },
       error: (err) => {
         console.error('Erreur chargement stats docType :', err);
--- src/app/admin/pages/stats-emprunts-user/stats-emprunts-user.component.ts
+++ src/app/admin/pages/stats-emprunts-user/stats-emprunts-user.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmpruntService } from '../../../services/emprunt.service';
 import { UserEmpruntStats } from '../../../models/emprunt.models';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-stats-emprunts-user',
@@ -8,19 +11,28 @@ import { UserEmpruntStats } from '../../../models/emprunt.models';
   styleUrls: ['./stats-emprunts-user.component.css']
 })
 export class StatsEmpruntsUserComponent implements OnInit {
-
-  stats: UserEmpruntStats[] = [];
+  displayedColumns: string[] = ['userId', 'userNom', 'userPrenom', 'totalEmprunts'];
+  dataSource = new MatTableDataSource<UserEmpruntStats>();
   errorMessage = '';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private empruntService: EmpruntService) {}
 
   ngOnInit(): void {
     this.empruntService.getStatsEmpruntsByUser().subscribe({
-      next: (data) => {
-        this.stats = data;
+      next: (data: UserEmpruntStats[]) => {
         if (data.length === 0) {
           this.errorMessage = 'Aucun résultat.';
+        } else {
+          this.errorMessage = '';
         }
+        this.dataSource.data = data;
+        setTimeout(() => {
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        });
       },
       error: (err) => {
         console.error('Erreur chargement stats user :', err);
